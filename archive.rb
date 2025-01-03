c89c53d6a0100f600e4f2bf73e789f48ca0c5353
Jose Aguirre <josito.aguirre@outlook.com>
2025-01-03 11:33:46 -0600

    add method add_song

--- archive.rb
+++ archive.rb
@@ -1,22 +1,28 @@
 class Archive
   def initialize
-    @songs = ["hola"]
+    @songs = {}
   end
 
   def archive
     puts "1. Add a song"
-    puts "2. Delete a song"
+    puts "2. List of songs"
     puts "3. Guess the song"
     puts "4. Exit"
     @answer = gets.chomp
     if @answer.to_i == 1
-     add_song
-     puts "Aloja... #{@songs}"
+      add_song
     end
 
   end
 
   def add_song
-    puts @songs
+    puts "Enter the song:"
+    new_song = gets.chomp
+    puts "Enter the singer"
+    singer = gets.chomp 
+
+    @songs.store(singer.to_sym, new_song)
+    print @songs
   end
+
 end
