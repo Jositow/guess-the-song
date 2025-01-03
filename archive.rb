22a0b893badbbd33aa20ed35e8b01ede8410196a
Jose Aguirre <josito.aguirre@outlook.com>
2025-01-03 09:52:49 -0600

    add method

--- archive.rb
+++ archive.rb
@@ -1,14 +1,22 @@
 class Archive
+  def initialize
+    @songs = ["hola"]
+  end
+
   def archive
     puts "1. Add a song"
     puts "2. Delete a song"
     puts "3. Guess the song"
     puts "4. Exit"
-
-    c = gets.chomp
-    puts "Your choice is #{c}."
+    @answer = gets.chomp
+    if @answer.to_i == 1
+     add_song
+     puts "Aloja... #{@songs}"
+    end
 
   end
 
-
+  def add_song
+    puts @songs
+  end
 end
