299abde315bc458e8761b9a79bbf03daadcb0c9a
Jose Aguirre <josito.aguirre@outlook.com>
2025-01-06 12:47:19 -0600

    add 2 tests

--- tests/test.rb
+++ tests/test.rb
@@ -1,1 +1,18 @@
-print "test"
+require 'minitest/autorun'
+require "./archive"
+require "./guess_song"
+
+
+class TestLab < Minitest::Test
+  def test_initilize
+    new = Archive.new
+    assert_equal [], new.instance_variable_get(:@songs)
+  end
+
+  def test_guess_song
+    now = NewGame.new("song", "singer")
+    assert_equal [], now.instance_variable_get(:@guessed_letters)
+    
+  end
+
+end
