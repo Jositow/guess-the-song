1e5cb37202169aad7d40e072d5e9b175929a3d40
Jose Aguirre <josito.aguirre@outlook.com>
2025-01-06 13:06:51 -0600

    change variable names in tests

--- tests/test.rb
+++ tests/test.rb
@@ -5,13 +5,13 @@ require "./guess_song"
 
 class TestLab < Minitest::Test
   def test_initilize
-    new = Archive.new
-    assert_equal [], new.instance_variable_get(:@songs)
+    test = Archive.new
+    assert_equal [], test.instance_variable_get(:@songs)
   end
 
   def test_guess_song
-    now = NewGame.new("song", "singer")
-    assert_equal [], now.instance_variable_get(:@guessed_letters)
+    test = NewGame.new("song", "singer")
+    assert_equal [], test.instance_variable_get(:@guessed_letters)
     
   end
 
